d3b7f4293d1e8632fe7155d8248f3e8b63ce7d26
Garrett Andrews <samsamthekat@gmail.com>
2016-11-28 15:57:32 -0600

    Update startSFTP.sh

--- ImageDetectionBackend/Location2/startSFTP.sh
+++ ImageDetectionBackend/Location2/startSFTP.sh
@@ -1,5 +1,5 @@
 # Make sure to first use SSH and set the RSA host, otherwise it wont work!!
-sshpass -p "Samismycat123" sftp gandrews@best-linux.cs.wisc.edu << !
+sshpass -p "*******insert password here*****" sftp USERNAME@best-linux.cs.wisc.edu << !
 	cd public
 	cd html
 	put testJSON2.json
